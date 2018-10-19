b6cd4f74c4cac3497c7fc919d0eb74a797cc95c9
Andrea Leone <git@andrealeone.xyz>
2018-10-19 12:11:37 +0200

    0.0.11

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,30 @@
 
-import Koa = require('Koa')
+import Koa       = require('koa')
+import Firebase  = require('firebase')
 
-const app = new Koa()
+const
+  app = new Koa(),
+  config = {
+    projectId:         "liam-ab541",
+    authDomain:        "liam-ab541.firebaseapp.com",
+    storageBucket:     "liam-ab541.appspot.com",
+    databaseURL:       "https://liam-ab541.firebaseio.com",
+    apiKey:            "AIzaSyArz2mYcusoD7zfX6y5cbguqWBS8HU7q34",
+    messagingSenderId: "111004570004"
+  }
+
+Firebase.initializeApp(config)
+
+app.use(async (ctx) => {
+
+  const
+    request = ctx.request,
+    response = ctx.response
+  
+  console.log(request)
+
+  ctx.body = 'Hello Koa'
+  
+})
+
+app.listen(3000)
